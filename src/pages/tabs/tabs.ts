c343ae664e785fc8a941087ded543eb7d1dc0d54
max <minxuan4787@gmail.com>
2018-12-11 21:03:14 +0800

    added a weather page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -18,11 +18,13 @@ export class TabsPage {
   tab1Root: string;
   tab2Root: string;
   tab3Root: string;
+  tab4Root: string;
 
   constructor(){
     this.tab1Root = 'InboxPage';
     this.tab2Root = 'ChannelsPage';
     this.tab3Root = 'ProfilePage';
+    this.tab3Root = 'WeatherPage';
   }
 
  
